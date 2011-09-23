59f863e402b0d877d796f347509382c9e1845142
Per <per.ersson@gmail.com>
2011-09-23 18:37:42 +0200

    Added a backgound image.

--- app/src/app/view/AppFrame.java
+++ app/src/app/view/AppFrame.java
@@ -1,11 +1,38 @@
 package app.view;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.net.URL;
 
 public class AppFrame extends Frame {
 
-    public AppFrame() {
-        setSize(640, 480);
+    private Image m_img;
 
+    @Override
+    public void paint(Graphics graphics) {
+        super.paint(graphics);    //To change body of overridden methods use File | Settings | File Templates.
+        Insets insets = getInsets();
+        graphics.drawImage(m_img, insets.left, insets.top, this);
+    }
+
+    public AppFrame() {
+
+
+        ClassLoader ld = ClassLoader.getSystemClassLoader();
+        URL url = ld.getResource("background.png");
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        m_img = tk.getImage(url);
+
+        setSize(1024, 563);
+
+        show();
+
+        addWindowListener(new WindowAdapter() {
+
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
     }
 }
