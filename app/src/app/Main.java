af3efe090c0fda0f72d2315cc141e0b81eacf8ea
Per <per.ersson@gmail.com>
2011-09-30 13:42:03 +0200

    Added default close operation.

--- app/src/app/Main.java
+++ app/src/app/Main.java
@@ -3,7 +3,7 @@ package app;
 import app.view.AppFrame;
 
 import javax.script.ScriptException;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 import java.io.IOException;
 
 public class Main {
@@ -12,7 +12,8 @@ public class Main {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    new AppFrame();
+                    JFrame frame = new AppFrame();
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 } catch (IOException e) {
                     e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
                 } catch (ScriptException e) {
