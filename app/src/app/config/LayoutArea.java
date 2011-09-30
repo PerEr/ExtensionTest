210f92f5b3cbc54f6246d9f3bc1a1f5e3d3ffccf
Per <per.ersson@gmail.com>
2011-09-30 11:30:52 +0200

    Added missing javadoc

--- app/src/app/config/LayoutArea.java
+++ app/src/app/config/LayoutArea.java
@@ -1,5 +1,12 @@
 package app.config;
 
+/**
+ * The interface exposed to javascript to add widgets to a layout
+ */
 public interface LayoutArea {
+    /**
+     * Add a widget by name
+     * @param widgetName The name of the widget
+     */
     void add(String widgetName);
 }
--- app/src/app/config/ScriptServices.java
+++ app/src/app/config/ScriptServices.java
@@ -1,8 +1,24 @@
 package app.config;
 
+/**
+ * The callback interface exposed to the javascript configuration file.
+ */
 public interface ScriptServices {
+    /**
+     * Register a plugin
+     * @param pluginName The plugin classname
+     */
     void register(String pluginName);
+
+    /**
+     * Do a two-phase load of the registered plugins, first "load" all, then let all resolve their dependencies.
+     */
     void loadAll();
 
+    /**
+     * Get hold of a named layout area
+     * @param str The name of the layout area
+     * @return A layout area that can be populated with widgets.
+     */
     LayoutArea getLayout(String str);
 }
